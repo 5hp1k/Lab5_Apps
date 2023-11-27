3dd652ae31e545fea9df2320fdc9393812319d94
5hpik <maxim.loboda2013@yandex.ru>
2023-11-27 11:04:55 +0700

    task3 и task4

--- task4/task4.py
+++ task4/task4.py
@@ -1,15 +1,36 @@
 import sys
+import random
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
+from PyQt5.QtCore import pyqtSignal
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
+
+class Button(QPushButton):
+    mouseMoved = pyqtSignal()
+
+    def mouseMoveEvent(self, event):
+        self.mouseMoved.emit()
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('task4.ui', self)
-        self.setFixedSize(800, 640)
+        self.coords = [40, 40]
+        self.btn_size = [120, 40]
+        self.d = 15
+        self.setGeometry(300, 300, 500, 400)
+        self.setWindowTitle('task4')
+
+        self.btn = Button(self)
+        self.btn.setMouseTracking(True)
+        self.btn.setText("Just a button")
+        self.btn.move(*self.coords)
+        self.btn.mouseMoved.connect(self.moveButton)
+        self.show()
+
+    def moveButton(self):
+            self.coords[0] = random.randint(0, 500 - self.btn_size[0])
+            self.coords[1] = random.randint(0, 400 - self.btn_size[1])
+            self.btn.move(*self.coords)
 
 
 def except_hook(cls, exception, traceback):
@@ -19,6 +40,6 @@ def except_hook(cls, exception, traceback):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
-    ex.show()
     sys.excepthook = except_hook
+    ex.show()
     sys.exit(app.exec_())
